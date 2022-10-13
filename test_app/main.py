928a275b82fca3ec38db18cd13894c2fe7dc117e
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2022-10-13 12:57:52 +0530

    Update main.py

--- test_app/main.py
+++ test_app/main.py
@@ -3,6 +3,9 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, NumericProperty
+from kivy.lang import Builder
+
+Builder.load_file("main.kv")
 
 class MainWidget(BoxLayout):
     
