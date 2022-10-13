7024ca0c8eb31c1ecdbffa5642fef55a174d8d8b
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2022-10-13 11:48:58 +0530

    Add files via upload

--- test_app/main.py
+++ test_app/main.py
@@ -1,17 +1,34 @@
-"""
-Simple Hello World app to test Buildozer Action.
-
-It builds Kivy app with file main.kv.
-"""
 
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, NumericProperty
+
+class MainWidget(BoxLayout):
+    
+    first_number = StringProperty("0")
+    second_number = StringProperty("0")
+    total = StringProperty("0")
+    current_math = ""
+
+    def text_enter(self, widget):
+        self.first_number = str(int(widget.text) + int(self.total))
+    
+    def enter(self, widget):
+        if self.current_math == "+":
+            self.first_number = self.second_number
+            self.total = str(int(self.first_number) + int(widget.text))
 
 
-class MainApp(App):
+    def add_number(self, widget):
+        self.current_math = "+"
+        self.first_number = self.total = widget.text
+        widget.text = "0"
+
+class MyCalcApp(App):
+    
     def build(self):
-        return Builder.load_file("main.kv")
-
+        return MainWidget()
 
 if __name__ == "__main__":
-    MainApp().run()
+    MyCalcApp().run()
