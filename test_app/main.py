750bf04d5143a715b839f55905560403781ca9cb
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2022-10-14 19:29:54 +0530

    Add files via upload

--- test_app/main.py
+++ test_app/main.py
@@ -1,45 +1,94 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.config import Config
+from kivy.utils import platform
 from kivy.properties import StringProperty, NumericProperty
+
+if platform != "android":
+    Config.set('graphics', 'width', '270')
+    Config.set('graphics', 'height', '575')
+Config.set('graphics', 'resizable', True)
+
+
+BG_NORMAL = "resources/Light rectangle.png"
+BG_DOWN = "resources/Dark rectangle.png"
 
 class MainWidget(BoxLayout):
     
     first_number = StringProperty("0")
-#    second_number = StringProperty("0")
+    second_number = StringProperty("0")
     total = StringProperty("0")
-    current_sign = ""
+    equation = StringProperty("0")
+    last_is_num = True
 
-    def text_enter(self, widget):
-        self.first_number = str(int(widget.text) + int(self.total))
+    def numeric_buttons(self, widget):
+        for one in range(9):
+            num_ = one + 1
+            btn_ = Button(text=str(num_))
+            btn_.background_normal = BG_NORMAL
+            btn_.background_down = BG_DOWN
+            btn_.border = (0, 0, 0, 0)
+            btn_.id = f"btn_{num_}"
+            btn_.font_size = 30
+            self.press_btn(btn_, num_)
+            widget.add_widget(btn_)
     
-    def clear(self):
-        self.total = "0"
+    def math_buttons(self, widget):
+        for sign in ["+", "-", "*", "/"]:
+            btn_ = Button(text=sign)
+            btn_.background_normal = BG_NORMAL
+            btn_.background_down = BG_DOWN
+            btn_.border = (0, 0, 0, 0)
+            btn_.font_size = 30
+            self.press_btn(btn_, sign)
+            widget.add_widget(btn_)
+    
+    def press_btn(self, btn, sign_or_num):
+        btn.on_press = lambda: self.pressed(sign_or_num)
 
-    def mult_number(self, widget):
-        first_number = str(widget.text)
-        total = str(float(self.total) * int(first_number))
-        self.total = str(int(total)) if isinstance(total, int) else str(float(total))
-        widget.text = str(0)
+    def pressed(self, sign_or_num):
+        if isinstance(sign_or_num, int):
+            self.amend_number(sign_or_num)
+        else:
+            self.amend_sign(sign_or_num)
+    
+    def amend_number(self, num):
+        if self.equation == "0":
+            self.equation = str(num)
+        else:
+            self.equation += str(num)
+        self.last_is_num = True
 
-    def div_number(self, widget):
-        first_number = str(widget.text)
-        total = str(float(self.total) / int(first_number))
-        self.total = str(int(total)) if isinstance(total, int) else str(float(total))
-        widget.text = str(0)
+    def amend_sign(self, sign):
+        if self.last_is_num:
+            self.equation += f" {sign} "
+            self.last_is_num = False
+        else:
+            self.equation = self.equation[:-3] + f" {sign} "
 
-    def add_number(self, widget):
-        first_number = str(widget.text)
-        total = str(float(self.total) + int(first_number))
-        self.total = str(int(total)) if isinstance(total, int) else str(float(total))
-        widget.text = str(0)
+    def plus_minus(self):
+        if self.equation.startswith("-"):
+            self.equation = self.equation.lstrip("-")
+        elif self.equation == "0":
+            pass
+        else:
+            self.equation = f"-{self.equation}"
 
-    def sub_number(self, widget):
-        first_number = str(widget.text)
-        total = str(float(self.total) - int(first_number))
-        self.total = str(int(total)) if isinstance(total, int) else str(float(total))
-        widget.text = str(0)
+    def amend_zero(self):
+        if self.equation == "0":
+            pass
+        else:
+            self.equation += "0"
+
+    def clear_equation(self):
+        self.equation = "0"
+    
+    def equal(self):
+        self.total = str(eval(self.equation))
+
 
 class MainApp(App):
     
